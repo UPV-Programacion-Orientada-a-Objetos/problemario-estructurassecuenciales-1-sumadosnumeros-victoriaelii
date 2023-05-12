13af3cf954c13b6f4bce67ffa9b2bdd24d60955b
Victoria <2130044@upv.edu.mx>
2023-05-12 00:52:01 -0600

    holi

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
 
     public static void main(String[] args) {
         int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1);
+        int b = Integer.parseInt(args[1]);
         SumaDosNumeros obj = new SumaDosNumeros();
         System.out.println(obj.suma(a,b));
         
